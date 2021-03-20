c265aaff60d45616bd1f0c5b30e2ab1b11c31f08
Frolov Kirill <Frolov.Kirill.17@yandex.ru>
2021-03-20 22:32:19 +0300

    Добавлена пауза

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_button_present_on_page(browser):
     browser.get(link)
-    time.sleep(0)
+    time.sleep(30)
     try:
         add_button = browser.find_element_by_class_name("btn-add-to-basket")
         check = True
