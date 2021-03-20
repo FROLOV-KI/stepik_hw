3f53bc27170c31dfa80956f9f39b7cafcf8b5f77
Frolov Kirill <Frolov.Kirill.17@yandex.ru>
2021-03-20 22:09:02 +0300

    Добавлен файл с фикстурами и файл с тестом, который проверяет наличие кнопки на странице

---
+++ test_items.py
@@ -0,0 +1,16 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_button_present_on_page(browser):
+    browser.get(link)
+    time.sleep(0)
+    try:
+        add_button = browser.find_element_by_class_name("btn-add-to-basket")
+        check = True
+    except:
+        check = False
+    assert check, "КНОПКА НЕ НАЙДЕНА!"
+    
+        
